1c6138c132d2b2f5717f7913d5350e961c926e09
Ashutosh Kumar Roy <ashu00619@gmail.com>
2023-08-08 02:06:51 +0530

    all responsives added and some css adjustments

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hello");
+const productItems = document.querySelectorAll('.product-item');
+
+productItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    productItems.forEach((item) => {
+      item.classList.remove('selected');
+    });
+    item.classList.add('selected');
+  });
+});
+
+// Function to show/hide the options based on the selected product
+function showOptions(selectedProductId) {
+    const options = document.querySelectorAll('.options');
+    options.forEach((option) => {
+      option.style.display = 'none';
+    });
+  
+    const selectedProductOptions = document.getElementById(`options${selectedProductId}`);
+    if (selectedProductOptions) {
+      selectedProductOptions.style.display = 'block';
+    }
+  }
+  
+  // Get the radio buttons and add event listeners to each
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  radioButtons.forEach((radio) => {
+    radio.addEventListener('click', () => {
+      const selectedProduct = document.querySelector('input[type="radio"]:checked');
+      const selectedProductId = selectedProduct.id.replace('product', '');
+      showOptions(selectedProductId);
+    });
+  });
